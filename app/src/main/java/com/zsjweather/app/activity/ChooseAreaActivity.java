2a67c298d1b5c4ec2cc4715ca5b676c33552e0a6
Downey <532720708@qq.com>
2016-02-27 17:07:39 +0800

    show information

--- app/src/main/java/com/zsjweather/app/activity/ChooseAreaActivity.java
+++ app/src/main/java/com/zsjweather/app/activity/ChooseAreaActivity.java
@@ -2,7 +2,10 @@ package com.zsjweather.app.activity;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.speech.tts.UtteranceProgressListener;
 import android.text.TextUtils;
 import android.view.View;
@@ -45,31 +48,6 @@ public class ChooseAreaActivity extends Activity {
     private Province selectedProvince;
     private City selectedCity;
     private int currentLevel;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.choose_area);
-        listView = (ListView) findViewById(R.id.list_view);
-        titleText = (TextView) findViewById(R.id.title_text);
-        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,dataList);
-        listView.setAdapter(adapter);
-        zsjWeatherDB = ZsjWeatherDB.getInstance(this);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> arg0, View view, int index, long arg3) {
-                if(currentLevel == LEVEL_PROVINCE){
-                    selectedProvince = provinceList.get(index);
-                    queryCities();
-                }else if(currentLevel == LEVEL_CITY){
-                    selectedCity = cityList.get(index);
-                    queryCounties();
-                }
-            }
-        });
-        queryProvinces();
-    }
 
     private void queryProvinces(){
         provinceList = zsjWeatherDB.loadProvinces();
@@ -193,6 +171,44 @@ public class ChooseAreaActivity extends Activity {
         }else {
             finish();
         }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if(prefs.getBoolean("city_selected",false)){
+            Intent intent = new Intent(this,WeatherActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.choose_area);
+        listView = (ListView)findViewById(R.id.list_view);
+        titleText = (TextView) findViewById(R.id.title_text);
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,dataList);
+        listView.setAdapter(adapter);
+        zsjWeatherDB = ZsjWeatherDB.getInstance(this);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?>arg0, View view, int index, long arg3) {
+                if (currentLevel == LEVEL_PROVINCE) {
+                    selectedProvince = provinceList.get(index);
+                    queryCities();
+                }else if (currentLevel == LEVEL_CITY) {
+                    selectedCity = cityList.get(index);
+                    queryCounties();
+                }else if (currentLevel == LEVEL_COUNTY){
+                    String countyCode = countyList.get(index).getCountyCode();
+                    Intent intent = new Intent(ChooseAreaActivity.this,WeatherActivity.class);
+                    intent.putExtra("county_code",countyCode);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        });
+        queryProvinces();
     }
 }
 
